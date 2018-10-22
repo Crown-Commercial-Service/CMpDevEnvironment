29a2962e1005a553fedfa206381b967a0a2d1f65
unknown <42997213+mkrowe@users.noreply.github.com>
2018-10-22 16:32:01 +0100

    Add callback and oauth flows

--- terraform/security/cognito.tf
+++ terraform/security/cognito.tf
@@ -61,7 +61,12 @@ resource "aws_cognito_user_pool_client" "ccs_user_pool_client" {
     name = "ccs_user_pool_client"
     user_pool_id = "${aws_cognito_user_pool.ccs_user_pool.id}"
     generate_secret = true 
-    explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
+    allowed_oauth_flows = ["code"]
+    allowed_oauth_flows_user_pool_client = true
+    allowed_oauth_scopes = ["openid", "email"]
+    callback_urls = ["https://cmp.cmpdev.roweitdev.co.uk/auth/cognito/callback"]
+    logout_urls = ["https://cmp.cmpdev.roweitdev.co.uk/"]
+    supported_identity_providers = ["COGNITO"]
 }
 
 ##############################################################
