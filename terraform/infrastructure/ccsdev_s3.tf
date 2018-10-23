d9faca29b363792d157b5c8c5d5af92def25af77
unknown <42997213+mkrowe@users.noreply.github.com>
2018-10-23 16:15:01 +0100

    Add Load Balancer logs into S3

--- terraform/infrastructure/ccsdev_s3.tf
+++ terraform/infrastructure/ccsdev_s3.tf
@@ -19,3 +19,57 @@ resource "aws_s3_bucket" "build-artifacts" {
     CCSEnvironment = "${var.environment_name}"
   }
 }
+
+##############################################################
+# Log Storage
+##############################################################
+data "aws_iam_policy_document" "log_policy_document" {
+  statement {
+
+    principals = {
+      type = "AWS"
+      identifiers = ["652711504416"] # eu-west-2 ALB ID
+    }
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = ["arn:aws:s3:::${local.log_bucket_name}/*"]
+  }
+}
+
+# resource "aws_s3_bucket_policy" "logs_policy" {
+#   bucket = "${aws_s3_bucket.logs.id}"
+#   policy = "${aws}"
+# }
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "${local.log_bucket_name}"
+  acl    = "private"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 5
+    }
+  }
+
+  lifecycle_rule {
+    enabled = true
+    prefix = "alb/" # Shorter rule for ALB logs
+
+    expiration {
+      days = 1
+    }
+  }
+
+  policy = "${data.aws_iam_policy_document.log_policy_document.json}"
+
+  tags {
+    Name = "CCSDEV Logs bucket"
+    CCSRole = "Infrastructure"
+    CCSEnvironment = "${var.environment_name}"
+  }
+}
