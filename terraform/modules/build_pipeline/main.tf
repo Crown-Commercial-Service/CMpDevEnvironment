866864cc6bb4827648d921597c7d67278f2871e4
Gary Ratcliffe <gary@roweit.co.uk>
2018-11-15 09:32:30 +0000

    The basic build pipeline does not support enable. If used the pipeline is aways enabled and thus the notification events/topics always need to be created.

--- terraform/modules/build_pipeline/main.tf
+++ terraform/modules/build_pipeline/main.tf
@@ -60,7 +60,7 @@ module "pipeline_events" {
   # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/pipeline_events"
   source = "../pipeline_events"
 
-  enable = "${var.enable}"
+  enable = true
   name = "${var.artifact_name}-pipeline"
 
   github_owner = "${var.github_owner}"
