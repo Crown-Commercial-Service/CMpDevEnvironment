ca9cef886f4712901b62dba3cf18a0cae6a38d3f
Gary Ratcliffe <gary@roweit.co.uk>
2018-11-29 15:12:38 +0000

    Added  definition for building marketplace application from production branch.

---
+++ terraform/build/crown-marketplace-production/main.tf
@@ -0,0 +1,34 @@
+module "component" {
+    # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
+    source = "../../modules/component"
+
+    environment_name = "Production"
+
+    type = "app"
+    prefix = "ccs"
+    name = "cmpprod"
+    hostname = "marketplace"
+    build_type = "custom"
+    build_image = "ccs/ruby"
+    github_owner = "Crown-Commercial-Service"
+    github_repo = "crown-marketplace"
+    # TODO Change this to production when branch is available.
+    github_branch = "master"
+    github_token_alias = "ccs-build_github_token"
+    cluster_name = "CCSDEV_app_cluster"
+    task_count = 3
+    enable_tests = true
+    enable_cognito_support = true
+    cognito_login_callback = "auth/cognito/callback"
+    environment = [
+      {
+        name = "RAILS_ENV",
+        value = "production"
+      },
+      {
+        name = "RAILS_SERVE_STATIC_FILES",
+        value = "true"
+      }
+    ]
+    port = "80"
+}
