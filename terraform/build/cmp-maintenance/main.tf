8ff3194a9b9ef845579388b886c1ed8ca24f305b
CCS TechOps <internal-it@crowncommercial.gov.uk>
2018-12-12 14:08:33 +0000

    Set Environment name to Production

--- terraform/build/cmp-maintenance/main.tf
+++ terraform/build/cmp-maintenance/main.tf
@@ -1,7 +1,7 @@
 module "component" {
     # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
     source = "../../modules/component"
-
+	environment_name = "Production"
     type = "app"
     prefix = "ccs"
     name = "maintenance"
