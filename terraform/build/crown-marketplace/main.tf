d36d328d65db0808b40b056a64f72ccf0af33941
Gary Ratcliffe <gary@roweit.co.uk>
2019-03-07 16:02:54 +0000

    Replaced single sandbox build pipeline with 5 to represent the different framework development strands. These are st, fm, mc, ls and ast representing supply teachers, facilities management, management consultancy, legal services, and apprenticeship training. These are all currently built from the master branch. The data upload capability is enabled for developer convenience. Pipelines must not be used ot deploy into the non-IP restricted envrionments with these settings.

--- terraform/build/crown-marketplace/main.tf
+++ terraform/build/crown-marketplace/main.tf
@@ -1,15 +1,18 @@
-module "component" {
+######################################
+# Supply Teachers
+######################################
+module "component-st" {
     # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
     source = "../../modules/component"
     environment_name = "Sandbox"
     type = "app"
     prefix = "ccs"
-    name = "cmp"
+    name = "st"
     build_type = "custom"
     build_image = "ccs/ruby"
     github_owner = "Crown-Commercial-Service"
     github_repo = "crown-marketplace"
-    github_branch = "sandbox"
+    github_branch = "master"
     github_token_alias = "ccs-build_github_token"
     cluster_name = "CCSDEV_app_cluster"
     task_count = 2
@@ -24,6 +27,171 @@ module "component" {
       },
       {
         name = "RAILS_SERVE_STATIC_FILES",
+        value = "true"
+      },
+      {
+        # Set to enable the ability to upload data
+        name = "APP_HAS_UPLOAD_PRIVILEGES",
+        value = "true"
+      }
+    ]
+    port = "80"
+}
+
+######################################
+# Facilities Management
+######################################
+module "component-fm" {
+    # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
+    source = "../../modules/component"
+    environment_name = "Sandbox"
+    type = "app"
+    prefix = "ccs"
+    name = "fm"
+    build_type = "custom"
+    build_image = "ccs/ruby"
+    github_owner = "Crown-Commercial-Service"
+    github_repo = "crown-marketplace"
+    github_branch = "master"
+    github_token_alias = "ccs-build_github_token"
+    cluster_name = "CCSDEV_app_cluster"
+    task_count = 2
+    enable_tests = true
+    enable_cognito_support = true
+    cognito_login_callback = "auth/cognito/callback"
+    cognito_logout_callback = "facilities-management/gateway"
+    environment = [
+      {
+        name = "RAILS_ENV",
+        value = "production"
+      },
+      {
+        name = "RAILS_SERVE_STATIC_FILES",
+        value = "true"
+      },
+      {
+        # Set to enable the ability to upload data
+        name = "APP_HAS_UPLOAD_PRIVILEGES",
+        value = "true"
+      }
+    ]
+    port = "80"
+}
+
+######################################
+# Management Consultancy
+######################################
+module "component-mc" {
+    # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
+    source = "../../modules/component"
+    environment_name = "Sandbox"
+    type = "app"
+    prefix = "ccs"
+    name = "mc"
+    build_type = "custom"
+    build_image = "ccs/ruby"
+    github_owner = "Crown-Commercial-Service"
+    github_repo = "crown-marketplace"
+    github_branch = "master"
+    github_token_alias = "ccs-build_github_token"
+    cluster_name = "CCSDEV_app_cluster"
+    task_count = 2
+    enable_tests = true
+    enable_cognito_support = true
+    cognito_login_callback = "auth/cognito/callback"
+    cognito_logout_callback = "management-consultancy/gateway"
+    environment = [
+      {
+        name = "RAILS_ENV",
+        value = "production"
+      },
+      {
+        name = "RAILS_SERVE_STATIC_FILES",
+        value = "true"
+      },
+      {
+        # Set to enable the ability to upload data
+        name = "APP_HAS_UPLOAD_PRIVILEGES",
+        value = "true"
+      }
+    ]
+    port = "80"
+}
+
+######################################
+# Apprenticeship Training
+######################################
+module "component-ats" {
+    # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
+    source = "../../modules/component"
+    environment_name = "Sandbox"
+    type = "app"
+    prefix = "ccs"
+    name = "ats"
+    build_type = "custom"
+    build_image = "ccs/ruby"
+    github_owner = "Crown-Commercial-Service"
+    github_repo = "crown-marketplace"
+    github_branch = "master"
+    github_token_alias = "ccs-build_github_token"
+    cluster_name = "CCSDEV_app_cluster"
+    task_count = 2
+    enable_tests = true
+    enable_cognito_support = true
+    cognito_login_callback = "auth/cognito/callback"
+    cognito_logout_callback = "apprenticeship-training/gateway"
+    environment = [
+      {
+        name = "RAILS_ENV",
+        value = "production"
+      },
+      {
+        name = "RAILS_SERVE_STATIC_FILES",
+        value = "true"
+      },
+      {
+        # Set to enable the ability to upload data
+        name = "APP_HAS_UPLOAD_PRIVILEGES",
+        value = "true"
+      }
+    ]
+    port = "80"
+}
+
+######################################
+# Legal Services
+######################################
+module "component-ls" {
+    # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
+    source = "../../modules/component"
+    environment_name = "Sandbox"
+    type = "app"
+    prefix = "ccs"
+    name = "ls"
+    build_type = "custom"
+    build_image = "ccs/ruby"
+    github_owner = "Crown-Commercial-Service"
+    github_repo = "crown-marketplace"
+    github_branch = "master"
+    github_token_alias = "ccs-build_github_token"
+    cluster_name = "CCSDEV_app_cluster"
+    task_count = 2
+    enable_tests = true
+    enable_cognito_support = true
+    cognito_login_callback = "auth/cognito/callback"
+    cognito_logout_callback = "legal-services/gateway"
+    environment = [
+      {
+        name = "RAILS_ENV",
+        value = "production"
+      },
+      {
+        name = "RAILS_SERVE_STATIC_FILES",
+        value = "true"
+      },
+      {
+        # Set to enable the ability to upload data
+        name = "APP_HAS_UPLOAD_PRIVILEGES",
         value = "true"
       }
     ]
