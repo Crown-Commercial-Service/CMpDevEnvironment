45322b76e67fb973ae3dbeff5aa4af49f537e37d
johndenford <63358032+johndenford@users.noreply.github.com>
2020-05-18 12:57:16 +0100

    Update techops_govnotify_parameters.tf

--- terraform/infrastructure/techops_govnotify_parameters.tf
+++ terraform/infrastructure/techops_govnotify_parameters.tf
@@ -7,7 +7,8 @@ resource "aws_ssm_parameter" "gov_notify_api_key" {
   description  = "Infrastructure configured Gov Notify API Key"
   type  = "SecureString"
   value = "ccsapplicationkey-ea34b16b-8fcc-4e8a-917c-fe546bb37ad1-902893a3-a810-4b15-985e-3ccad3aea1c7"
-
+  overwrite = true
+  
   tags {
     Name = "Parameter Store: Infrastructure configured Gov Notify API Key"
     CCSRole = "Infrastructure"
