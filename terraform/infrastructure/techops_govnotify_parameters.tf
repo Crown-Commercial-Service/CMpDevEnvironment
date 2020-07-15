0e2c969f3bfd21f044947e4e11ebfe99ee752e5b
Paul Illingworth <paul.illingworth@roweit.co.uk>
2020-07-15 16:57:29 +0100

    Replace value with placeholder text.

--- terraform/infrastructure/techops_govnotify_parameters.tf
+++ terraform/infrastructure/techops_govnotify_parameters.tf
@@ -6,7 +6,7 @@ resource "aws_ssm_parameter" "gov_notify_api_key" {
   name  = "/Environment/ccs/cmp/GOV_NOTIFY_API_KEY"
   description  = "Infrastructure configured Gov Notify API Key"
   type  = "SecureString"
-  value = "ccsapplicationkey-ea34b16b-8fcc-4e8a-917c-fe546bb37ad1-902893a3-a810-4b15-985e-3ccad3aea1c7"
+  value = "the_gov_notify_api_key_value_would_go_in_here"
   overwrite = true
   
   tags {
