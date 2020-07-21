ae7ce8b320401c837df9c189f24def1920ccd4fb
Paul Illingworth <paul.illingworth@roweit.co.uk>
2020-07-21 13:45:57 +0100

    Spelling and formatting

--- terraform/modules/routing/main.tf
+++ terraform/modules/routing/main.tf
@@ -7,22 +7,21 @@
 ##############################################################
 # "Proxy" provider; provider must be passed into the module
 ##############################################################
-provider "aws" {
-}
+provider "aws" {}
 
 ##############################################################
 # Load Balancer configuration
 ##############################################################
 locals {
   provision_https_certificates = "${upper(var.protocol) == "HTTPS" ? 1 : 0}"
-  has_path_pattern = "${var.path_pattern == "" ? 0 : 1}"
+  has_path_pattern             = "${var.path_pattern == "" ? 0 : 1}"
 
   # it seems that even though we are not applying a path pattern
   # (via count = 0) we can't default to an empty string as the
   # condition:path_pattern doesn't like empty values, hence the "/"
   # but it could be any value as it is not used
   path_pattern = "${local.has_path_pattern ? var.path_pattern : "/"}"
-  
+
   # only register the DNS record if this is host based routing only i.e
   # this does not have a path_pattern
   register_dns_record = "${local.has_path_pattern ? 0 : 1}"
@@ -52,7 +51,7 @@ resource "aws_alb_target_group" "component" {
 }
 
 resource "aws_alb_listener_rule" "http_subdomain_rule" {
-  count = "${local.provision_https_certificates == 0 ? 1 : 0}"
+  count        = "${local.provision_https_certificates == 0 ? 1 : 0}"
   listener_arn = "${data.aws_alb_listener.http_listener.arn}"
 
   action {
@@ -88,14 +87,15 @@ resource "aws_alb_listener_rule" "http_subdomain_catchall_rule" {
 }
 
 resource "aws_alb_listener_rule" "http_subdomain_redirect_rule" {
-  count = "${local.provision_https_certificates}"
+  count        = "${local.provision_https_certificates}"
   listener_arn = "${data.aws_alb_listener.http_listener.arn}"
 
   action {
     type = "redirect"
+
     redirect {
-      port = "443"
-      protocol = "HTTPS"
+      port        = "443"
+      protocol    = "HTTPS"
       status_code = "HTTP_301"
     }
   }
@@ -108,7 +108,6 @@ resource "aws_alb_listener_rule" "http_subdomain_redirect_rule" {
 }
 
 resource "aws_alb_listener_rule" "http_subdomain_redirect_catchall_rule" {
-
   count = "${local.provision_https_certificates && var.catch_all ? 1 : 0}"
 
   # catch_all comes after all other rules
@@ -118,9 +117,10 @@ resource "aws_alb_listener_rule" "http_subdomain_redirect_catchall_rule" {
 
   action {
     type = "redirect"
+
     redirect {
-      port = "443"
-      protocol = "HTTPS"
+      port        = "443"
+      protocol    = "HTTPS"
       status_code = "HTTP_301"
     }
   }
@@ -133,13 +133,13 @@ resource "aws_alb_listener_rule" "http_subdomain_redirect_catchall_rule" {
 }
 
 data "aws_alb_listener" "https_listener" {
-  count = "${local.provision_https_certificates}"
+  count             = "${local.provision_https_certificates}"
   load_balancer_arn = "${data.aws_alb.CCSDEV_cluster_alb.arn}"
-  port = "443"
+  port              = "443"
 }
 
 resource "aws_alb_listener_rule" "https_subdomain_rule_hostname" {
-  count = "${local.provision_https_certificates && !local.has_path_pattern ? 1 : 0}"
+  count        = "${local.provision_https_certificates && !local.has_path_pattern ? 1 : 0}"
   listener_arn = "${data.aws_alb_listener.https_listener.arn}"
 
   # just a hostname match needs to come after the more specific hostname and path matching
@@ -158,14 +158,14 @@ resource "aws_alb_listener_rule" "https_subdomain_rule_hostname" {
 }
 
 resource "aws_alb_listener_rule" "https_subdomain_rule_hostname_and_path" {
-  count = "${local.provision_https_certificates && local.has_path_pattern ? 1 : 0}"
+  count        = "${local.provision_https_certificates && local.has_path_pattern ? 1 : 0}"
   listener_arn = "${data.aws_alb_listener.https_listener.arn}"
 
-  # this rule comes before the other host/pathy/catch_all rules
+  # this rule comes before the other host/path/catch_all rules
   priority = 1000
 
   action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = "${aws_alb_target_group.component.arn}"
   }
 
@@ -180,11 +180,9 @@ resource "aws_alb_listener_rule" "https_subdomain_rule_hostname_and_path" {
       values = ["${local.path_pattern}"]
     }
   }
-
 }
 
 resource "aws_alb_listener_rule" "https_subdomain_catchall_rule" {
-
   count = "${local.provision_https_certificates && var.catch_all ? 1 : 0}"
 
   # catch_all comes after all other rules
@@ -193,7 +191,7 @@ resource "aws_alb_listener_rule" "https_subdomain_catchall_rule" {
   listener_arn = "${data.aws_alb_listener.https_listener.arn}"
 
   action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = "${aws_alb_target_group.component.arn}"
   }
 
@@ -203,7 +201,6 @@ resource "aws_alb_listener_rule" "https_subdomain_catchall_rule" {
     }
   }
 }
-
 
 ##############################################################
 # DNS configuration
@@ -216,8 +213,8 @@ resource "aws_route53_record" "component" {
   type    = "A"
 
   alias {
-    name = "${data.aws_alb.CCSDEV_cluster_alb.dns_name}"
-    zone_id = "${data.aws_alb.CCSDEV_cluster_alb.zone_id}"
+    name                   = "${data.aws_alb.CCSDEV_cluster_alb.dns_name}"
+    zone_id                = "${data.aws_alb.CCSDEV_cluster_alb.zone_id}"
     evaluate_target_health = true
   }
 }
