946f7477e386ea6bc677c22cc51e67109fd70e4f
Paul Illingworth <paul.illingworth@roweit.co.uk>
2020-07-21 15:43:32 +0100

    Remove whitespace chnages
    To improve chance of successful cross branch merging.

--- terraform/build/api2/main.tf
+++ terraform/build/api2/main.tf
@@ -3,27 +3,27 @@ terraform {
 }
 
 module "component" {
-  # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
-  source = "../../modules/component"
+    # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
+    source = "../../modules/component"
 
-  type = "api"
-  prefix = "ccs"
-  name = "api2"
-  build_type = "python"
-  github_owner = "Crown-Commercial-Service"
-  github_repo = "CMpExampleApi2"
-  github_branch = "master"
-  github_token_alias = "ccs-build_github_token"
-  cluster_name = "CCSDEV_api_cluster"
-  task_count = 1
-  environment = [
-    {
-      name = "CCS_FEATURE_EG1",
-      value = "on"
-    } 
-  ]
-  port = "80"
-  providers = {
-    aws = "aws"
-  }    
+    type = "api"
+    prefix = "ccs"
+    name = "api2"
+    build_type = "python"
+    github_owner = "Crown-Commercial-Service"
+    github_repo = "CMpExampleApi2"
+    github_branch = "master"
+    github_token_alias = "ccs-build_github_token"
+    cluster_name = "CCSDEV_api_cluster"
+    task_count = 1
+    environment = [
+      {
+        name = "CCS_FEATURE_EG1",
+        value = "on"
+      } 
+    ]
+    port = "80"
+    providers = {
+      aws = "aws"
+    }    
 }
--- terraform/build/crown-marketplace/main.tf
+++ terraform/build/crown-marketplace/main.tf
@@ -3,37 +3,37 @@ terraform {
 }
 
 module "component" {
-  # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
-  source = "../../modules/component"
+    # source = "git::https://github.com/Crown-Commercial-Service/CMpDevEnvironment.git//terraform/modules/component"
+    source = "../../modules/component"
 
-  type = "app"
-  prefix = "ccs"
-  name = "cmp"
-  hostname = "cmp"
-  path_pattern = "/facilitiesmanagement"
-  build_type = "custom"
-  build_image = "ccs/ruby"
-  github_owner = "Crown-Commercial-Service"
-  github_repo = "crown-marketplace"
-  github_branch = "master"
-  github_token_alias = "ccs-build_github_token"
-  cluster_name = "CCSDEV_app_cluster"
-  task_count = 2
-  autoscaling_max_count = 4
-  enable_tests = true
-  enable_cognito_api_support = true
-  environment = [
-    {
-      name = "RAILS_ENV",
-      value = "production"
-    },
-    {
-      name = "RAILS_SERVE_STATIC_FILES",
-      value = "true"
-    }
-  ]
-  port = "80"
-  providers = {
-    aws = "aws"
-  }    
+    type = "app"
+    prefix = "ccs"
+    name = "cmp"
+    hostname = "cmp"
+    path_pattern = "/facilitiesmanagement"
+    build_type = "custom"
+    build_image = "ccs/ruby"
+    github_owner = "Crown-Commercial-Service"
+    github_repo = "crown-marketplace"
+    github_branch = "master"
+    github_token_alias = "ccs-build_github_token"
+    cluster_name = "CCSDEV_app_cluster"
+    task_count = 2
+    autoscaling_max_count = 4
+    enable_tests = true
+    enable_cognito_api_support = true
+    environment = [
+      {
+        name = "RAILS_ENV",
+        value = "production"
+      },
+      {
+        name = "RAILS_SERVE_STATIC_FILES",
+        value = "true"
+      }
+    ]
+    port = "80"
+    providers = {
+      aws = "aws"
+    }    
 }
