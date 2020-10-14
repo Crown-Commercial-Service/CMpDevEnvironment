7250ec99ebc5808c21496996b9d923cc50535a23
George <george.hill@crowncommercial.gov.uk>
2020-10-14 11:24:21 +0100

    Bump AWS DB Instance Engine version to 11.8

--- terraform/infrastructure/ccsdev_database.tf
+++ terraform/infrastructure/ccsdev_database.tf
@@ -132,7 +132,7 @@ resource "aws_db_instance" "ccsdev_default_db" {
   apply_immediately         = "${var.default_db_apply_immediately}"
   storage_type              = "gp2"
   engine                    = "postgres"
-  engine_version            = "11.2"
+  engine_version            = "11.8"
   instance_class            = "${var.default_db_instance_class}"
   name                      = "${var.default_db_name}"
   username                  = "${var.default_db_username}"
