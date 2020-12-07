e9728fb72114ccf929eef42729c95e0aedf50aed
George <george.hill@crowncommercial.gov.uk>
2020-12-07 13:23:43 +0000

    Create logging bucket, and enable logging on origin bucket

--- terraform/ithc/locals.tf
+++ terraform/ithc/locals.tf
@@ -2,4 +2,5 @@ data "aws_caller_identity" "current" {}
 
 locals {
   ithc_test_bucket_name = "ccs.${data.aws_caller_identity.current.account_id}.ithctests3bucket"
+  s3_logging_bucket_name = "ccs.${data.aws_caller_identity.current.account_id}.s3.access-logs"
 }
--- terraform/ithc/main.tf
+++ terraform/ithc/main.tf
@@ -1,5 +1,27 @@
+resource "aws_s3_bucket" "test_logging_bucket" {
+  acl    = "log-delivery-write"
+  bucket = "${local.s3_logging_bucket_name}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
 resource "aws_s3_bucket" "ithc_test_bucket_name" {
   bucket = "${local.ithc_test_bucket_name}"
+
+  logging {
+    target_bucket = "${aws_s3_bucket.test_logging_bucket.id}"
+    target_prefix = "log/"
+  }
 
   server_side_encryption_configuration {
     rule {
