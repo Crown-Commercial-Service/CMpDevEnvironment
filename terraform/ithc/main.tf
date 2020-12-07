476b9e484a729ba5039bd3b1b2db585f8c7a7201
George <george.hill@crowncommercial.gov.uk>
2020-12-07 11:45:36 +0000

    Enable default encryption

--- terraform/ithc/main.tf
+++ terraform/ithc/main.tf
@@ -1,6 +1,14 @@
 resource "aws_s3_bucket" "ithc_test_bucket_name" {
   bucket = "${local.ithc_test_bucket_name}"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   versioning {
     enabled = true
   }
