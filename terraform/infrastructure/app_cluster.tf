495f6e6e38eb7f47d271eedc0d769a8fc4caffbc
cosborneCCS <57792536+cosborneCCS@users.noreply.github.com>
2020-12-08 14:38:33 +0000

    Update app_cluster.tf

--- terraform/infrastructure/app_cluster.tf
+++ terraform/infrastructure/app_cluster.tf
@@ -89,7 +89,7 @@ resource "aws_alb_listener" "CCSDEV_app_cluster_alb_listener_https" {
   load_balancer_arn = "${aws_alb.CCSDEV_app_cluster_alb.arn}"
   port              = "${var.https_port}"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-FS-2018-06"
+  ssl_policy        = "ELBSecurityPolicy-FS-1-2-2019-08"
   certificate_arn   = "${aws_acm_certificate.public_cluster_wildcard_certificate.arn}"
 
   default_action {
