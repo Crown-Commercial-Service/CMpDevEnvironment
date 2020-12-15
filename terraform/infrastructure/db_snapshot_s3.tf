e9ed05bfb4ee9b94c096b4b5f63c50286bff1c12
George <george.hill@crowncommercial.gov.uk>
2020-12-15 13:34:39 +0000

    Implement ITHC changes for db snapshot bucket (previously wasnt captured via Terraform)

---
+++ terraform/infrastructure/db_snapshot_s3.tf
@@ -0,0 +1,67 @@
+resource "aws_s3_bucket" "db_snapshot_s3" {
+  bucket = "${local.db_snapshot_bucket_name}"
+
+  logging {
+    target_bucket = "${data.aws_s3_bucket.s3_logging_bucket.id}"
+    target_prefix = "Logs/"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+data "aws_iam_policy_document" "db_snapshot_s3" {
+  statement {
+    effect = "Deny"
+
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    actions = [
+      "s3:*",
+    ]
+
+    condition {
+      test = "Bool"
+
+      values = [
+        "false",
+      ]
+
+      variable = "aws:SecureTransport"
+    }
+
+    resources = [
+      "${aws_s3_bucket.db_snapshot_s3.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "config_bucket_s3" {
+  bucket = "${aws_s3_bucket.db_snapshot_s3.bucket}"
+  policy = "${data.aws_iam_policy_document.db_snapshot_s3.json}"
+}
+
+resource "aws_s3_bucket_public_access_block" "db_snapshot_public_access_block" {
+  bucket = "${aws_s3_bucket.db_snapshot_s3.id}"
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+locals {
+  db_snapshot_bucket_name = "${data.aws_caller_identity.current.account_id}-dbsnapshot"
+}
