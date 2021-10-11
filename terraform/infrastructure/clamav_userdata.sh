391fe9566f522037b86677a5144431796421095b
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2021-10-11 15:33:51 +0100

    tf code for clamav

---
+++ terraform/infrastructure/clamav_userdata.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+server_ip=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
+
+aws ssm put-parameter --name /Environment/global/CLAMAV_SERVER_ADDRESS --value $server_ip --type String --overwrite --region eu-west-2
+aws ssm put-parameter --name /Environment/global/CLAMAV_SERVER_IP --value $server_ip --type String --overwrite --region eu-west-2
+
+sudo systemctl enable clamd@scan.service
+sudo systemctl start clamd@scan.service
