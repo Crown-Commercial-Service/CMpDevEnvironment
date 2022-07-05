f94b3074df368d193c49ff4af798938dbcb275e1
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2022-07-05 10:08:08 +0000

    Add in region to command

--- terraform/infrastructure/bastion_userdata.sh
+++ terraform/infrastructure/bastion_userdata.sh
@@ -9,4 +9,4 @@ instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 allocation_id=eipalloc-0baf119ca82747098
 region=eu-west-2
 
-aws ec2 associate-address --instance-id $instance_id --allocation-id $allocation_id --allow-reassociation
+aws ec2 associate-address --instance-id $instance_id --allocation-id $allocation_id --allow-reassociation --region $region
