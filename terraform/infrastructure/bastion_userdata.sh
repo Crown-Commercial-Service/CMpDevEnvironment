462d8c013d92955e76e85b247cc3a02a9dc6e0f5
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2022-07-05 11:43:41 +0000

    Add in jq

--- terraform/infrastructure/bastion_userdata.sh
+++ terraform/infrastructure/bastion_userdata.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Install jq
+yum install jq -y
+
 # Update ssh ciphers and restart ssh service
 echo Ciphers ${ssh_ciphers} | sudo tee -a /etc/ssh/sshd_config
 systemctl restart sshd
@@ -7,6 +10,6 @@ systemctl restart sshd
 # Associate elastic ip to instance
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 region=eu-west-2
-allocation_id=$(aws ssm get-parameter --name /eip/allocation-id --region $region)
+allocation_id=$(aws ssm get-parameter --name /eip/allocation-id --region $region | jq -r '.Parameter.Value')
 
 aws ec2 associate-address --instance-id $instance_id --allocation-id $allocation_id --allow-reassociation --region $region
